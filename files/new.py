0a861b843d86f3d76b47708850000685a3fa691b
Sanjana Wijenayake <56200533+SanaDev98@users.noreply.github.com>
2024-08-28 00:43:29 +0530

    densenet

---
+++ files/new.py
@@ -0,0 +1,249 @@
+import argparse
+import os
+from math import log10
+import time
+import psutil
+import GPUtil
+import json
+from collections import defaultdict
+
+import pandas as pd
+import torch
+import torch.optim as optim
+import torch.utils.data
+import torchvision.utils as utils
+from torch.utils.data import DataLoader, Subset
+from tqdm import tqdm
+import random
+
+import pytorch_ssim
+from data_utils import TrainDatasetFromFolder, ValDatasetFromFolder, display_transform
+from loss import GeneratorLoss
+from model import Generator, Discriminator
+
+base_path = os.path.dirname(__file__)
+
+parser = argparse.ArgumentParser(description='Train Super Resolution Models')
+parser.add_argument('--crop_size', default=88, type=int, help='training images crop size')
+parser.add_argument('--upscale_factor', default=4, type=int, choices=[2, 4, 8],
+                    help='super resolution upscale factor')
+parser.add_argument('--num_epochs', default=100, type=int, help='train epoch number')
+parser.add_argument('--train_sample_count', default=20000, type=int, help='number of training samples to use')
+
+def get_performance_metrics():
+    gpus = GPUtil.getGPUs()
+    gpu_util = gpus[0].load * 100 if gpus else 0
+    gpu_mem_mb = gpus[0].memoryUsed if gpus else 0
+    gpu_mem_gb = gpu_mem_mb / 1024 if gpu_mem_mb else 0
+    cpu_util = psutil.cpu_percent()
+    ram = psutil.virtual_memory()
+    ram_util = ram.percent
+    ram_used_mb = ram.used / (1024 * 1024)  # Convert bytes to MB
+    ram_used_gb = ram_used_mb / 1024  # Convert MB to GB
+    return {
+        'gpu_util': gpu_util,
+        'gpu_mem_mb': gpu_mem_mb,
+        'gpu_mem_gb': gpu_mem_gb,
+        'cpu_util': cpu_util,
+        'ram_util': ram_util,
+        'ram_used_mb': ram_used_mb,
+        'ram_used_gb': ram_used_gb,
+    }
+
+if __name__ == '__main__':
+    opt = parser.parse_args()
+    
+    CROP_SIZE = opt.crop_size
+    UPSCALE_FACTOR = opt.upscale_factor
+    NUM_EPOCHS = opt.num_epochs
+    TRAIN_SAMPLE_COUNT = opt.train_sample_count
+    
+    train_set = TrainDatasetFromFolder(os.path.join(base_path, "data/train"), crop_size=CROP_SIZE, upscale_factor=UPSCALE_FACTOR)
+    
+    if TRAIN_SAMPLE_COUNT and len(train_set) > TRAIN_SAMPLE_COUNT:
+        indices = random.sample(range(len(train_set)), TRAIN_SAMPLE_COUNT)
+        train_set = Subset(train_set, indices)
+    
+    val_set = ValDatasetFromFolder(os.path.join(base_path, "data/val"), upscale_factor=UPSCALE_FACTOR)
+    train_loader = DataLoader(dataset=train_set, num_workers=4, batch_size=64, shuffle=True)
+    val_loader = DataLoader(dataset=val_set, num_workers=4, batch_size=1, shuffle=False)
+    
+    netG = Generator(UPSCALE_FACTOR)
+    print('# generator parameters:', sum(param.numel() for param in netG.parameters()))
+    netD = Discriminator()
+    print('# discriminator parameters:', sum(param.numel() for param in netD.parameters()))
+    
+    generator_criterion = GeneratorLoss()
+    
+    if torch.cuda.is_available():
+        netG.cuda()
+        netD.cuda()
+        generator_criterion.cuda()
+    
+    optimizerG = optim.Adam(netG.parameters())
+    optimizerD = optim.Adam(netD.parameters())
+    
+    results = {'d_loss': [], 'g_loss': [], 'd_score': [], 'g_score': [], 'psnr': [], 'ssim': []}
+    performance_metrics = defaultdict(list)
+    
+    for epoch in range(1, NUM_EPOCHS + 1):
+        epoch_start_time = time.time()
+        train_bar = tqdm(train_loader)
+        running_results = {'batch_sizes': 0, 'd_loss': 0, 'g_loss': 0, 'd_score': 0, 'g_score': 0}
+    
+        netG.train()
+        netD.train()
+        
+        batch_metrics = []
+        
+        for batch_num, (data, target) in enumerate(train_bar):
+            batch_start_time = time.time()
+            g_update_first = True
+            batch_size = data.size(0)
+            running_results['batch_sizes'] += batch_size
+
+            real_img = target
+            if torch.cuda.is_available():
+                real_img = real_img.float().cuda()
+            z = data
+            if torch.cuda.is_available():
+                z = z.float().cuda()
+            fake_img = netG(z)
+            fake_out = netD(fake_img).mean()
+
+            optimizerG.zero_grad()
+            g_loss = generator_criterion(fake_out, fake_img, real_img)
+            g_loss.backward()
+            optimizerG.step()
+
+            real_out = netD(real_img).mean()
+            fake_out = netD(fake_img.detach()).mean()
+            d_loss = 1 - real_out + fake_out
+
+            optimizerD.zero_grad()
+            d_loss.backward()
+            
+            fake_img = netG(z)
+            fake_out = netD(fake_img).mean()
+
+            optimizerD.step()
+
+            running_results['g_loss'] += g_loss.item() * batch_size
+            running_results['d_loss'] += d_loss.item() * batch_size
+            running_results['d_score'] += real_out.item() * batch_size
+            running_results['g_score'] += fake_out.item() * batch_size
+    
+            train_bar.set_description(desc='[%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f' % (
+                epoch, NUM_EPOCHS, running_results['d_loss'] / running_results['batch_sizes'],
+                running_results['g_loss'] / running_results['batch_sizes'],
+                running_results['d_score'] / running_results['batch_sizes'],
+                running_results['g_score'] / running_results['batch_sizes']))
+            
+            batch_metrics.append({
+                'batch_num': batch_num,
+                'g_loss': g_loss.item(),
+                'd_loss': d_loss.item(),
+                'd_score': real_out.item(),
+                'g_score': fake_out.item(),
+                'batch_time': time.time() - batch_start_time,
+                **get_performance_metrics()
+            })
+    
+        netG.eval()
+        out_path = 'training_results/SRF_' + str(UPSCALE_FACTOR) + '/'
+        if not os.path.exists(out_path):
+            os.makedirs(out_path)
+        
+        with torch.no_grad():
+            val_bar = tqdm(val_loader)
+            valing_results = {'mse': 0, 'ssims': 0, 'psnr': 0, 'ssim': 0, 'batch_sizes': 0}
+            val_images = []
+            for val_lr, val_hr_restore, val_hr in val_bar:
+                batch_size = val_lr.size(0)
+                valing_results['batch_sizes'] += batch_size
+                lr = val_lr
+                hr = val_hr
+                if torch.cuda.is_available():
+                    lr = lr.float().cuda()
+                    hr = hr.float().cuda()
+                sr = netG(lr)
+        
+                batch_mse = ((sr - hr) ** 2).data.mean()
+                valing_results['mse'] += batch_mse * batch_size
+                batch_ssim = pytorch_ssim.ssim(sr, hr).item()
+                valing_results['ssims'] += batch_ssim * batch_size
+                valing_results['psnr'] = 10 * log10((hr.max()**2) / (valing_results['mse'] / valing_results['batch_sizes']))
+                valing_results['ssim'] = valing_results['ssims'] / valing_results['batch_sizes']
+                val_bar.set_description(
+                    desc='[converting LR images to SR images] PSNR: %.4f dB SSIM: %.4f' % (
+                        valing_results['psnr'], valing_results['ssim']))
+        
+                val_images.extend(
+                    [display_transform()(val_hr_restore.squeeze(0)), display_transform()(hr.data.cpu().squeeze(0)),
+                     display_transform()(sr.data.cpu().squeeze(0))])
+            
+            # Save only 10 validation image results for every 20 epochs
+            if epoch % 20 == 0:
+                val_images = torch.stack(val_images[:30])  # Take only first 10 sets (30 images)
+                val_images = torch.chunk(val_images, val_images.size(0) // 3)
+                val_save_bar = tqdm(val_images, desc='[saving validation results]')
+                for i, image in enumerate(val_save_bar):
+                    image = utils.make_grid(image, nrow=3, padding=5)
+                    utils.save_image(image, out_path + 'epoch_%d_img_%d.png' % (epoch, i+1), padding=5)
+    
+        save_dir = 'epochs'
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        torch.save(netG.state_dict(), os.path.join(save_dir, 'netG_epoch_%d_%d.pth' % (UPSCALE_FACTOR, epoch)))
+        torch.save(netD.state_dict(), os.path.join(save_dir, 'netD_epoch_%d_%d.pth' % (UPSCALE_FACTOR, epoch)))
+        
+        results['d_loss'].append(running_results['d_loss'] / running_results['batch_sizes'])
+        results['g_loss'].append(running_results['g_loss'] / running_results['batch_sizes'])
+        results['d_score'].append(running_results['d_score'] / running_results['batch_sizes'])
+        results['g_score'].append(running_results['g_score'] / running_results['batch_sizes'])
+        results['psnr'].append(valing_results['psnr'])
+        results['ssim'].append(valing_results['ssim'])
+    
+        epoch_time = time.time() - epoch_start_time
+        epoch_metrics = {
+            'epoch': epoch,
+            'epoch_time': epoch_time,
+            'd_loss': results['d_loss'][-1],
+            'g_loss': results['g_loss'][-1],
+            'd_score': results['d_score'][-1],
+            'g_score': results['g_score'][-1],
+            'psnr': results['psnr'][-1],
+            'ssim': results['ssim'][-1],
+            **{k: sum(d[k] for d in batch_metrics) / len(batch_metrics) for k in ['gpu_util', 'gpu_mem_mb', 'gpu_mem_gb', 'cpu_util', 'ram_util', 'ram_used_mb', 'ram_used_gb']}
+        }
+        performance_metrics['epoch'].append(epoch)
+        for k, v in epoch_metrics.items():
+            if k != 'epoch':
+                performance_metrics[k].append(v)
+        
+        stats_dir = 'statistics'
+        if not os.path.exists(stats_dir):
+            os.makedirs(stats_dir)
+        
+        # Save performance metrics every epoch
+        perf_df = pd.DataFrame(performance_metrics)
+        perf_df.to_csv(os.path.join(stats_dir, 'performance_metrics.csv'), index=False)
+        
+        # Save batch metrics
+        with open(os.path.join(stats_dir, f'epoch_{epoch}_batch_metrics.json'), 'w') as f:
+            json.dump(batch_metrics, f)
+        
+        if epoch % 10 == 0 and epoch != 0:
+            data_frame = pd.DataFrame(
+                data={'Loss_D': results['d_loss'], 'Loss_G': results['g_loss'], 'Score_D': results['d_score'],
+                      'Score_G': results['g_score'], 'PSNR': results['psnr'], 'SSIM': results['ssim']},
+                index=range(1, epoch + 1))
+            out_path = os.path.join(stats_dir, 'srf_' + str(UPSCALE_FACTOR) + '_train_results.csv')
+            data_frame.to_csv(out_path, index_label='Epoch')
+
+        print(f"Epoch {epoch} completed. Time taken: {epoch_time:.2f}s")
+        print(f"Average metrics - GPU Util: {epoch_metrics['gpu_util']:.2f}%, "
+              f"GPU Mem: {epoch_metrics['gpu_mem_mb']:.2f}MB ({epoch_metrics['gpu_mem_gb']:.2f}GB), "
+              f"CPU Util: {epoch_metrics['cpu_util']:.2f}%, "
+              f"RAM Util: {epoch_metrics['ram_util']:.2f}%, "
+              f"RAM Used: {epoch_metrics['ram_used_mb']:.2f}MB ({epoch_metrics['ram_used_gb']:.2f}GB)")
---
+++ loss.py
@@ -0,0 +1,405 @@
+
+
+#EfficientNet
+###########################################################################################################################################################
+
+
+# import torch
+# from torch import nn
+# from torchvision.models import efficientnet_b0
+
+# class GeneratorLoss(nn.Module):
+#     def __init__(self):
+#         super(GeneratorLoss, self).__init__()
+#         # Load pre-trained EfficientNet model
+#         efficientnet = efficientnet_b0(pretrained=True)
+        
+#         # Extract features from EfficientNet
+#         # The `features` part of EfficientNet includes all convolutional layers
+#         # EfficientNet doesn't have a single `features` attribute like DenseNet or VGG,
+#         # so we'll use the `features` from the `features` layer directly.
+#         self.loss_network = efficientnet.features.eval()
+        
+#         # Freeze the parameters of the feature extractor
+#         for param in self.loss_network.parameters():
+#             param.requires_grad = False
+            
+#         # Define the loss functions
+#         self.mse_loss = nn.MSELoss()
+#         self.tv_loss = TVLoss()
+
+#     def forward(self, out_labels, out_images, target_images):
+#         # Adversarial Loss
+#         adversarial_loss = torch.mean(1 - out_labels)
+        
+#         # Perception Loss (using features from EfficientNet)
+#         perception_loss = self.mse_loss(self.loss_network(out_images), self.loss_network(target_images))
+        
+#         # Image Loss
+#         image_loss = self.mse_loss(out_images, target_images)
+        
+#         # TV Loss (Total Variation Loss)
+#         tv_loss = self.tv_loss(out_images)
+        
+#         # Combine all losses
+#         return image_loss + 0.001 * adversarial_loss + 0.006 * perception_loss + 2e-8 * tv_loss
+
+
+# class TVLoss(nn.Module):
+#     def __init__(self, tv_loss_weight=1):
+#         super(TVLoss, self).__init__()
+#         self.tv_loss_weight = tv_loss_weight
+
+#     def forward(self, x):
+#         batch_size = x.size()[0]
+#         h_x = x.size()[2]
+#         w_x = x.size()[3]
+#         count_h = self.tensor_size(x[:, :, 1:, :])
+#         count_w = self.tensor_size(x[:, :, :, 1:])
+#         h_tv = torch.pow((x[:, :, 1:, :] - x[:, :, :h_x - 1, :]), 2).sum()
+#         w_tv = torch.pow((x[:, :, :, 1:] - x[:, :, :, :w_x - 1]), 2).sum()
+#         return self.tv_loss_weight * 2 * (h_tv / count_h + w_tv / count_w) / batch_size
+
+#     @staticmethod
+#     def tensor_size(t):
+#         return t.size()[1] * t.size()[2] * t.size()[3]
+
+
+# if __name__ == "__main__":
+#     # Initialize the GeneratorLoss
+#     g_loss = GeneratorLoss()
+    
+#     # Print the GeneratorLoss instance
+#     print(g_loss)
+    
+#     # Example input tensors
+#     batch_size = 1
+#     channels = 3
+#     height = 224
+#     width = 224
+    
+#     out_labels = torch.rand(batch_size, 1)  # Random adversarial labels
+#     out_images = torch.rand(batch_size, channels, height, width)  # Generated images
+#     target_images = torch.rand(batch_size, channels, height, width)  # Target high-res images
+    
+#     # Calculate the loss
+#     loss = g_loss(out_labels, out_images, target_images)
+    
+#     # Print the calculated loss
+#     print("Calculated Loss:", loss.item())
+
+
+
+#DenseNet
+###########################################################################################################################################################
+
+
+
+import torch
+from torch import nn
+from torchvision.models import densenet121
+
+class GeneratorLoss(nn.Module):
+    def __init__(self):
+        super(GeneratorLoss, self).__init__()
+        # Load pre-trained DenseNet model
+        densenet = densenet121(pretrained=True)
+        
+        # Use all the convolutional layers (features) from DenseNet
+        self.loss_network = densenet.features.eval()
+        
+        # Freeze the parameters of the feature extractor
+        for param in self.loss_network.parameters():
+            param.requires_grad = False
+            
+        # Define the loss functions
+        self.mse_loss = nn.MSELoss()
+        self.tv_loss = TVLoss()
+
+    def forward(self, out_labels, out_images, target_images):
+        # Adversarial Loss
+        adversarial_loss = torch.mean(1 - out_labels)
+        
+        # Perception Loss (using features from DenseNet)
+        perception_loss = self.mse_loss(self.loss_network(out_images), self.loss_network(target_images))
+        
+        # Image Loss
+        image_loss = self.mse_loss(out_images, target_images)
+        
+        # TV Loss (Total Variation Loss)
+        tv_loss = self.tv_loss(out_images)
+        
+        # Combine all losses
+        return image_loss + 0.001 * adversarial_loss + 0.006 * perception_loss + 2e-8 * tv_loss
+
+
+class TVLoss(nn.Module):
+    def __init__(self, tv_loss_weight=1):
+        super(TVLoss, self).__init__()
+        self.tv_loss_weight = tv_loss_weight
+
+    def forward(self, x):
+        batch_size = x.size()[0]
+        h_x = x.size()[2]
+        w_x = x.size()[3]
+        count_h = self.tensor_size(x[:, :, 1:, :])
+        count_w = self.tensor_size(x[:, :, :, 1:])
+        h_tv = torch.pow((x[:, :, 1:, :] - x[:, :, :h_x - 1, :]), 2).sum()
+        w_tv = torch.pow((x[:, :, :, 1:] - x[:, :, :, :w_x - 1]), 2).sum()
+        return self.tv_loss_weight * 2 * (h_tv / count_h + w_tv / count_w) / batch_size
+
+    @staticmethod
+    def tensor_size(t):
+        return t.size()[1] * t.size()[2] * t.size()[3]
+
+
+if __name__ == "__main__":
+    # Initialize the GeneratorLoss
+    g_loss = GeneratorLoss()
+    
+    # Print the GeneratorLoss instance
+    print(g_loss)
+    
+    # Example input tensors
+    batch_size = 1
+    channels = 3
+    height = 224
+    width = 224
+    
+    out_labels = torch.rand(batch_size, 1)  # Random adversarial labels
+    out_images = torch.rand(batch_size, channels, height, width)  # Generated images
+    target_images = torch.rand(batch_size, channels, height, width)  # Target high-res images
+    
+    # Calculate the loss
+    loss = g_loss(out_labels, out_images, target_images)
+    
+    # Print the calculated loss
+    print("Calculated Loss:", loss.item())
+
+
+
+
+#VGG16
+###########################################################################################################################################################
+
+
+# import torch
+# from torch import nn
+# from torchvision.models.vgg import vgg16
+
+
+# class GeneratorLoss(nn.Module):
+#     def __init__(self):
+#         super(GeneratorLoss, self).__init__()
+#         vgg = vgg16(pretrained=True)
+#         loss_network = nn.Sequential(*list(vgg.features)[:31]).eval()
+#         for param in loss_network.parameters():
+#             param.requires_grad = False
+#         self.loss_network = loss_network
+#         self.mse_loss = nn.MSELoss()
+#         self.tv_loss = TVLoss()
+
+#     def forward(self, out_labels, out_images, target_images):
+#         # Adversarial Loss
+#         adversarial_loss = torch.mean(1 - out_labels)
+#         # Perception Loss
+#         perception_loss = self.mse_loss(self.loss_network(out_images), self.loss_network(target_images))
+#         # Image Loss
+#         image_loss = self.mse_loss(out_images, target_images)
+#         # TV Loss
+#         tv_loss = self.tv_loss(out_images)
+#         return image_loss + 0.001 * adversarial_loss + 0.006 * perception_loss + 2e-8 * tv_loss
+
+
+# class TVLoss(nn.Module):
+#     def __init__(self, tv_loss_weight=1):
+#         super(TVLoss, self).__init__()
+#         self.tv_loss_weight = tv_loss_weight
+
+#     def forward(self, x):
+#         batch_size = x.size()[0]
+#         h_x = x.size()[2]
+#         w_x = x.size()[3]
+#         count_h = self.tensor_size(x[:, :, 1:, :])
+#         count_w = self.tensor_size(x[:, :, :, 1:])
+#         h_tv = torch.pow((x[:, :, 1:, :] - x[:, :, :h_x - 1, :]), 2).sum()
+#         w_tv = torch.pow((x[:, :, :, 1:] - x[:, :, :, :w_x - 1]), 2).sum()
+#         return self.tv_loss_weight * 2 * (h_tv / count_h + w_tv / count_w) / batch_size
+
+#     @staticmethod
+#     def tensor_size(t):
+#         return t.size()[1] * t.size()[2] * t.size()[3]
+
+
+# if __name__ == "__main__":
+#     g_loss = GeneratorLoss()
+#     print(g_loss)
+
+
+#MobileNet2
+###########################################################################################################################################################
+
+# import torch
+# from torch import nn
+# from torchvision.models import mobilenet_v2
+
+# class GeneratorLoss(nn.Module):
+#     def __init__(self):
+#         super(GeneratorLoss, self).__init__()
+#         mobilenet = mobilenet_v2(pretrained=True)
+#         # We use the features (excluding the classifier) of MobileNetV2
+#         loss_network = mobilenet.features.eval()
+#         for param in loss_network.parameters():
+#             param.requires_grad = False
+#         self.loss_network = loss_network
+#         self.mse_loss = nn.MSELoss()
+#         self.tv_loss = TVLoss()
+
+#     def forward(self, out_labels, out_images, target_images):
+#         # Adversarial Loss
+#         adversarial_loss = torch.mean(1 - out_labels)
+#         # Perception Loss
+#         perception_loss = self.mse_loss(self.loss_network(out_images), self.loss_network(target_images))
+#         # Image Loss
+#         image_loss = self.mse_loss(out_images, target_images)
+#         # TV Loss
+#         tv_loss = self.tv_loss(out_images)
+#         return image_loss + 0.001 * adversarial_loss + 0.006 * perception_loss + 2e-8 * tv_loss
+
+
+# class TVLoss(nn.Module):
+#     def __init__(self, tv_loss_weight=1):
+#         super(TVLoss, self).__init__()
+#         self.tv_loss_weight = tv_loss_weight
+
+#     def forward(self, x):
+#         batch_size = x.size()[0]
+#         h_x = x.size()[2]
+#         w_x = x.size()[3]
+#         count_h = self.tensor_size(x[:, :, 1:, :])
+#         count_w = self.tensor_size(x[:, :, :, 1:])
+#         h_tv = torch.pow((x[:, :, 1:, :] - x[:, :, :h_x - 1, :]), 2).sum()
+#         w_tv = torch.pow((x[:, :, :, 1:] - x[:, :, :, :w_x - 1]), 2).sum()
+#         return self.tv_loss_weight * 2 * (h_tv / count_h + w_tv / count_w) / batch_size
+
+#     @staticmethod
+#     def tensor_size(t):
+#         return t.size()[1] * t.size()[2] * t.size()[3]
+
+
+# if __name__ == "__main__":
+#     g_loss = GeneratorLoss()
+#     print(g_loss)
+
+
+
+# ResNet50
+##########################################################################################################################################################
+
+# import torch
+# from torch import nn
+# from torchvision.models import resnet50
+
+# class GeneratorLoss(nn.Module):
+#     def __init__(self):
+#         super(GeneratorLoss, self).__init__()
+#         resnet = resnet50(pretrained=True)
+#         # We use the layers up to the 4th block of ResNet50 for feature extraction
+#         loss_network = nn.Sequential(*list(resnet.children())[:8]).eval()
+#         for param in loss_network.parameters():
+#             param.requires_grad = False
+#         self.loss_network = loss_network
+#         self.mse_loss = nn.MSELoss()
+#         self.tv_loss = TVLoss()
+
+#     def forward(self, out_labels, out_images, target_images):
+#         # Adversarial Loss
+#         adversarial_loss = torch.mean(1 - out_labels)
+#         # Perception Loss
+#         perception_loss = self.mse_loss(self.loss_network(out_images), self.loss_network(target_images))
+#         # Image Loss
+#         image_loss = self.mse_loss(out_images, target_images)
+#         # TV Loss
+#         tv_loss = self.tv_loss(out_images)
+#         return image_loss + 0.001 * adversarial_loss + 0.006 * perception_loss + 2e-8 * tv_loss
+
+
+# class TVLoss(nn.Module):
+#     def __init__(self, tv_loss_weight=1):
+#         super(TVLoss, self).__init__()
+#         self.tv_loss_weight = tv_loss_weight
+
+#     def forward(self, x):
+#         batch_size = x.size()[0]
+#         h_x = x.size()[2]
+#         w_x = x.size()[3]
+#         count_h = self.tensor_size(x[:, :, 1:, :])
+#         count_w = self.tensor_size(x[:, :, :, 1:])
+#         h_tv = torch.pow((x[:, :, 1:, :] - x[:, :, :h_x - 1, :]), 2).sum()
+#         w_tv = torch.pow((x[:, :, :, 1:] - x[:, :, :, :w_x - 1]), 2).sum()
+#         return self.tv_loss_weight * 2 * (h_tv / count_h + w_tv / count_w) / batch_size
+
+#     @staticmethod
+#     def tensor_size(t):
+#         return t.size()[1] * t.size()[2] * t.size()[3]
+
+
+# if __name__ == "__main__":
+#     g_loss = GeneratorLoss()
+#     print(g_loss)
+
+
+#VGG19
+###########################################################################################################################################################
+
+# import torch
+# from torch import nn
+# from torchvision.models import vgg19
+
+# class GeneratorLoss(nn.Module):
+#     def __init__(self):
+#         super(GeneratorLoss, self).__init__()
+#         vgg = vgg19(pretrained=True)
+#         # We use the first 36 layers of VGG19 (up to 'conv4_4')
+#         loss_network = nn.Sequential(*list(vgg.features)[:36]).eval()
+#         for param in loss_network.parameters():
+#             param.requires_grad = False
+#         self.loss_network = loss_network
+#         self.mse_loss = nn.MSELoss()
+#         self.tv_loss = TVLoss()
+
+#     def forward(self, out_labels, out_images, target_images):
+#         # Adversarial Loss
+#         adversarial_loss = torch.mean(1 - out_labels)
+#         # Perception Loss
+#         perception_loss = self.mse_loss(self.loss_network(out_images), self.loss_network(target_images))
+#         # Image Loss
+#         image_loss = self.mse_loss(out_images, target_images)
+#         # TV Loss
+#         tv_loss = self.tv_loss(out_images)
+#         return image_loss + 0.001 * adversarial_loss + 0.006 * perception_loss + 2e-8 * tv_loss
+
+
+# class TVLoss(nn.Module):
+#     def __init__(self, tv_loss_weight=1):
+#         super(TVLoss, self).__init__()
+#         self.tv_loss_weight = tv_loss_weight
+
+#     def forward(self, x):
+#         batch_size = x.size()[0]
+#         h_x = x.size()[2]
+#         w_x = x.size()[3]
+#         count_h = self.tensor_size(x[:, :, 1:, :])
+#         count_w = self.tensor_size(x[:, :, :, 1:])
+#         h_tv = torch.pow((x[:, :, 1:, :] - x[:, :, :h_x - 1, :]), 2).sum()
+#         w_tv = torch.pow((x[:, :, :, 1:] - x[:, :, :, :w_x - 1]), 2).sum()
+#         return self.tv_loss_weight * 2 * (h_tv / count_h + w_tv / count_w) / batch_size
+
+#     @staticmethod
+#     def tensor_size(t):
+#         return t.size()[1] * t.size()[2] * t.size()[3]
+
+
+# if __name__ == "__main__":
+#     g_loss = GeneratorLoss()
+#     print(g_loss)
