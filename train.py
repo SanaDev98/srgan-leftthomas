401ad9501c676374119d055d3d3fd18ab53809a7
Sanjana Wijenayake <hasankasanjana@gmail.com>
2024-12-20 00:00:10 +0530

    gray

--- train.py
+++ train.py
@@ -61,14 +61,14 @@ if __name__ == '__main__':
     # Add this line to set the desired number of training samples
     TRAIN_SAMPLE_COUNT = 20000  # Change this to your desired number
     
-    train_set = TrainDatasetFromFolder('/kaggle/input/painting-train', crop_size=CROP_SIZE, upscale_factor=UPSCALE_FACTOR)
+    train_set = TrainDatasetFromFolder('/kaggle/input/face-grayscale-validation', crop_size=CROP_SIZE, upscale_factor=UPSCALE_FACTOR)
     
     # Limit the number of training samples
     if len(train_set) > TRAIN_SAMPLE_COUNT:
         indices = random.sample(range(len(train_set)), TRAIN_SAMPLE_COUNT)
         train_set = Subset(train_set, indices)
     
-    val_set = ValDatasetFromFolder('/kaggle/input/painting-validation', upscale_factor=UPSCALE_FACTOR)
+    val_set = ValDatasetFromFolder('/kaggle/input/grasdad', upscale_factor=UPSCALE_FACTOR)
     train_loader = DataLoader(dataset=train_set, num_workers=4, batch_size=64, shuffle=True)
     val_loader = DataLoader(dataset=val_set, num_workers=4, batch_size=1, shuffle=False)
     
